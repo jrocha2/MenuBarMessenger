00e18c689f8d6e2e3aaff840cb333258a112a408
John Rocha <john.rocha5@gmail.com>
2016-01-09 13:23:04 -0600

    Initial project commit

---
+++ Menu Bar Messenger/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Menu Bar Messenger
+//
+//  Created by John Rocha on 1/9/16.
+//  Copyright © 2016 John Rocha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
