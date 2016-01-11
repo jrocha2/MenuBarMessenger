08eceb415a17cacb8529c595aceefd86563a8ae7
John Rocha <john.rocha5@gmail.com>
2016-01-11 11:30:36 -0500

    Created Event Monitor class that checks for actions outside of the app and closes popover accordingly

--- Menu Bar Messenger/AppDelegate.swift
+++ Menu Bar Messenger/AppDelegate.swift
@@ -12,6 +12,7 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     let popover = NSPopover()
+    var eventMonitor: EventMonitor?
     
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
@@ -22,22 +23,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem.action = Selector("togglePopover:")
         
         popover.contentViewController = TabViewController(nibName: "TabViewController", bundle: nil)
-    }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        
+        // Close popover if left or right click detected outside of this application
+        eventMonitor = EventMonitor(mask: [.LeftMouseDownMask, .RightMouseDownMask]) { [unowned self] event in
+            if self.popover.shown {
+                self.closePopover(event)
+            }
+        }
+        eventMonitor?.start()
     }
     
+    // Toggles the popover view coming from the menu bar button
     func togglePopover(sender: AnyObject?) {
         if popover.shown {
-            popover.performClose(sender)
+            closePopover(sender)
         } else {
-            if let button = statusItem.button {
-                popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
-            }
+            openPopover(sender)
         }
     }
     
+    // If menu bar button pressed, open popover under that button
+    func openPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
+        eventMonitor?.start()
+    }
     
+    // Close the popover 
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+        eventMonitor?.stop()
+    }
 }
 
---
+++ Menu Bar Messenger/EventMonitor.swift
@@ -0,0 +1,36 @@
+//
+//  EventMonitor.swift
+//  Menu Bar Messenger
+//
+//  Created by John Rocha on 1/11/16.
+//  Copyright © 2016 John Rocha. All rights reserved.
+//
+//  Class that allows you to initialize event monitor for specified events
+
+import Cocoa
+
+public class EventMonitor {
+    private var monitor: AnyObject?
+    private let mask: NSEventMask
+    private let handler: NSEvent? -> ()
+    
+    public init(mask: NSEventMask, handler: NSEvent? -> ()) {
+        self.mask = mask
+        self.handler = handler
+    }
+    
+    deinit {
+        stop()
+    }
+    
+    public func start() {
+        monitor = NSEvent.addGlobalMonitorForEventsMatchingMask(mask, handler: handler)
+    }
+    
+    public func stop() {
+        if monitor != nil {
+            NSEvent.removeMonitor(monitor!)
+            monitor = nil
+        }
+    }
+}
