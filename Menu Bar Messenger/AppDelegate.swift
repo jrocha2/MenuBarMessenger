7df9669837f0714ffd9dfc083afc2fd25448deec
John Rocha <john.rocha5@gmail.com>
2016-01-10 21:08:08 -0600

    Initialized project with temporary menu bar icon

--- Menu Bar Messenger/AppDelegate.swift
+++ Menu Bar Messenger/AppDelegate.swift
@@ -7,21 +7,27 @@
 //
 
 import Cocoa
+import WebKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let icon = NSImage(named: "statusIcon")
+        icon!.template = true   // Changes icon a bit if in dark mode
+        
+        statusItem.image = icon
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
